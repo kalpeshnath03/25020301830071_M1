31c7e5ed928662fd13df65b545ccfb3bef5deb9c
kalpeshnath03 <devdakalpesh03@gmail.com>
2026-05-19 14:01:10 +0530

    Added all files

---
+++ DAY4/javascript (2).js
@@ -0,0 +1,24 @@
+// alert("byby ")
+// document.getElementById("demo").innerHTML = "Hello JavaScript!";
+var x = 5;
+var y = 6;
+var z = x + y;
+var p = x * y;
+var q = x - y;
+var r = x / y;
+document.getElementById("demo").innerHTML = "value of z is:" + z;
+document.getElementById("demo1").innerHTML = "value of p is:" + p;
+document.getElementById("demo2").innerHTML = "value of q is:" + q;
+document.getElementById("demo3").innerHTML = "value of r is:" + r;
+document.write("<br>type of p is:" + typeof (p));
+document.write("<br>boolean value is:" + Boolean(10 < 9));
+
+// object
+let person = { firstname: "Shivi", lastname: "Singh", age: 23, haircolour: "Black" };
+console.log(person.firstname);
+
+
+// Array
+let colors = ["red", "green", "blue"];
+console.log(colors[1]); 
+
