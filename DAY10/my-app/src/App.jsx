04e2d1ed9e4ff53321089306b8592174c60ed0fb
kalpeshnath03 <devdakalpesh03@gmail.com>
2026-05-21 12:47:37 +0530

    add all the files

---
+++ DAY10/my-app/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import Hello from './Hello'
+// import Bye from './Bye'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Day 9 - React Router</h1>
+      <Hello />
+      {/* <Bye /> */}
+      {/* <img src={reactLogo} /> */}
+
+    </>
+  )
+}
+
+
+
+export default App
---
+++ DAY10/my-app/src/Hello.jsx
@@ -0,0 +1,35 @@
+// import '/.App.css'
+
+function Hello() {
+
+    const getName = (yourname) => {
+        return yourname || 'Kalpesh'
+    }
+
+    function handleClick() {
+        alert('Button Clicked')
+    }
+    const handleMouseOver = () => { 
+        console.log("Mouse Over")
+    }
+    const name = 'Kalpesh'
+    const name2 = 'kalpeshnath'
+    const  handleInput=(event) => {
+        console.clear()
+        console.log("Value", event.target.value)
+    }
+    return (
+        <>
+            <h2>Hello {getName(name)}</h2>
+            <h2>bye {getName(name2)}</h2>
+            <p onMouseOver={handleMouseOver} onDoubleClick={handleDoubleClick}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <button onClick={handleClick}>Click Me</button>   
+            <button onClick={() => alert("hello from inline function!")}>say hello</button>
+        <br />
+        <input type="text" onChange={handleInput} placeholder="type something" />
+
+        </>
+    )
+
+}
+export default Hello 
