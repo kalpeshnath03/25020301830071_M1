5e2c16fda2670e6878be6a61275f3599ed2d514e
kalpeshnath03 <devdakalpesh03@gmail.com>
2026-05-26 13:27:57 +0530

    add all the files

--- DAY12/Expressjs/index.js
+++ DAY12/Expressjs/index.js
@@ -12,3 +12,17 @@ app.listen(3000, () => {
 app.get('/',(req,res) => {
     res.send('Hello ');
 });
+
+app.get('/about',(req,res) => {
+    res.send('Hello from the About page!');
+});
+app.get('/user/:userid/book/:bookid',(req,res) => {
+    res.send(`<h1>User ID: ${req.params.userid}, Book ID: ${req.params.bookid}</h1>`);
+});
+
+app.get('/search',(req,res) => {
+    const name = req.query.name;
+    const age = req.query.age;
+    const city = req.query.city;
+    res.send(`Search Results for Name: ${name}, Age: ${age}, City: ${city}`);
+}); 
